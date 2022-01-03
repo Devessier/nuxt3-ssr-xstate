878df8c4cb5ccb7908d6ecc41301e5c720365eaf
Devessier <baptiste@devessier.fr>
2022-01-03 19:31:12 +0100

    feat: create basic state machine and wait for it to settle during ssr

--- app.vue
+++ app.vue
@@ -1,9 +1,55 @@
 <script lang="ts" setup>
-</script>
+import { interpret } from "xstate/lib/interpreter";
+import { createModel } from "xstate/lib/model";
 
+const model = createModel(
+  {},
+  {
+    events: {},
+  }
+);
+
+const machine = model.createMachine({
+  initial: 'pending',
+  
+  states: {
+    pending: {
+      after: {
+        1_000: {
+          target: "fetched",
+        },
+      },
+    },
+
+    fetched: {
+      tags: "settled",
+    },
+  },
+});
+
+const { data } = await useAsyncData("machine", () => {
+  return new Promise((resolve) => {
+    const service = interpret(machine)
+      .onTransition((state) => {
+        if (state.hasTag("settled")) {
+          resolve({state});
+
+          service.stop();
+
+          return;
+        }
+      })
+      .start();
+  });
+});
+</script>
 
 <template>
   <div>
     <NuxtWelcome />
+
+    <p>
+      {{data.state.value}}
+    </p>
   </div>
 </template>
